288a051d979e7132a181a3a11b0d276cd9fbd3cb
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2022-09-27 14:02:42 +0200

    update

--- js/app.js
+++ js/app.js
@@ -11,10 +11,73 @@ selections.forEach((selection) => {
         const cInput = cOptions[Math.floor(Math.random() * 3)];
 
         updateMoveImgs(pInput, cInput);
+        compareInputs(pInput, cInput);
+        updateScore();
+        if(checkWin()) {
+            pScore = cScore = 0;
+        }
     });
 });
 
 function updateMoveImgs(pInput, cInput) {
     document.getElementById("p-move").src = `./img/${pInput}.svg`;
     document.getElementById("c-move").src = `./img/${cInput}.svg`;
-}s
+}
+
+function compareInputs(pInput, cInput) {
+    if(pInput === cInput) {
+        document.getElementById("result-text").innerHTML = "It is a tie!";
+        return;
+
+
+    }
+
+    if(pInput === "rock"){
+        if(cInput == "scissors") {
+            document.getElementById("result-text").innerHTML = "You win!";
+            pScore++;
+        }
+        else {
+            document.getElementById("result-text").innerHTML = "Computer win!";
+            cScore++;
+        }
+    }
+
+    if(pInput === "paper"){
+        if(cInput == "rock") {
+            document.getElementById("result-text").innerHTML = "You win!";
+            pScore++;
+        }
+        else {
+            document.getElementById("result-text").innerHTML = "Computer win!";
+            cScore++;
+        }
+    }
+    // pInput == "scissors"
+    else {
+        if(cInput == "paper") {
+            document.getElementById("result-text").innerHTML = "You win!";
+            pScore++;
+        }
+        else {
+            document.getElementById("result-text").innerHTML = "Computer win!";
+            cScore++;
+        }
+    }
+}
+
+function updateScore() {
+    document.getElementById("p-score").innerHTML = pScore;
+    document.getElementById("c-score").innerHTML = cScore;
+}
+
+function checkWin() {
+    if(pScore === 5 || cScore === 5) {
+        const winner  = pScore === 5
+            ? "You win the game! Congratulations!"
+            : "Computer wins the game. Better luck nest time";
+        document.getElementById("result-text").innerHTML = winner;
+        return true;
+    }
+    return false;
+}
